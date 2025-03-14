3adc6e303dc4f0c4294881d566703c8fde37024e
dexxhawk <Dsshnyakin@yandex.ru>
2025-03-15 00:06:33 +0300

    update event

--- ml_service/ml_service/internal/events/kafka_pb2.py
+++ ml_service/ml_service/internal/events/kafka_pb2.py
@@ -24,7 +24,7 @@ _sym_db = _symbol_database.Default()
 
 
 
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n&ml_service/internal/events/kafka.proto\"\xf2\x02\n\x0cUziProcessed\x12\x0e\n\x06uzi_id\x18\x64 \x01(\t\x12<\n\x13nodes_with_segments\x18\xc8\x01 \x03(\x0b\x32\x1e.UziProcessed.NodeWithSegments\x1aL\n\x04Node\x12\n\n\x02\x61i\x18\x64 \x01(\x08\x12\x12\n\ttirads_23\x18\x90\x03 \x01(\x01\x12\x11\n\x08tirads_4\x18\xf4\x03 \x01(\x01\x12\x11\n\x08tirads_5\x18\xd8\x04 \x01(\x01\x1a\x66\n\x07Segment\x12\x10\n\x08image_id\x18\x64 \x01(\t\x12\x0f\n\x06\x63ontor\x18\xc8\x01 \x01(\x0c\x12\x12\n\ttirads_23\x18\xac\x02 \x01(\x01\x12\x11\n\x08tirads_4\x18\x90\x03 \x01(\x01\x12\x11\n\x08tirads_5\x18\xf4\x03 \x01(\x01\x1a^\n\x10NodeWithSegments\x12 \n\x04node\x18\x64 \x01(\x0b\x32\x12.UziProcessed.Node\x12(\n\x08segments\x18\xc8\x01 \x03(\x0b\x32\x15.UziProcessed.SegmentB.Z,internal/generated/dbus/consume/uziprocessedb\x06proto3')
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n&ml_service/internal/events/kafka.proto\"0\n\x0bUziSplitted\x12\x0e\n\x06uzi_id\x18\x64 \x01(\t\x12\x11\n\x08pages_id\x18\xc8\x01 \x03(\t\"\xf2\x02\n\x0cUziProcessed\x12\x0e\n\x06uzi_id\x18\x64 \x01(\t\x12<\n\x13nodes_with_segments\x18\xc8\x01 \x03(\x0b\x32\x1e.UziProcessed.NodeWithSegments\x1aL\n\x04Node\x12\n\n\x02\x61i\x18\x64 \x01(\x08\x12\x12\n\ttirads_23\x18\x90\x03 \x01(\x01\x12\x11\n\x08tirads_4\x18\xf4\x03 \x01(\x01\x12\x11\n\x08tirads_5\x18\xd8\x04 \x01(\x01\x1a\x66\n\x07Segment\x12\x10\n\x08image_id\x18\x64 \x01(\t\x12\x0f\n\x06\x63ontor\x18\xc8\x01 \x01(\x0c\x12\x12\n\ttirads_23\x18\xac\x02 \x01(\x01\x12\x11\n\x08tirads_4\x18\x90\x03 \x01(\x01\x12\x11\n\x08tirads_5\x18\xf4\x03 \x01(\x01\x1a^\n\x10NodeWithSegments\x12 \n\x04node\x18\x64 \x01(\x0b\x32\x12.UziProcessed.Node\x12(\n\x08segments\x18\xc8\x01 \x03(\x0b\x32\x15.UziProcessed.SegmentB.Z,internal/generated/dbus/consume/uziprocessedb\x06proto3')
 
 _globals = globals()
 _builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, _globals)
@@ -32,12 +32,14 @@ _builder.BuildTopDescriptorsAndMessages(DESCRIPTOR, 'ml_service.internal.events.
 if not _descriptor._USE_C_DESCRIPTORS:
   _globals['DESCRIPTOR']._loaded_options = None
   _globals['DESCRIPTOR']._serialized_options = b'Z,internal/generated/dbus/consume/uziprocessed'
-  _globals['_UZIPROCESSED']._serialized_start=43
-  _globals['_UZIPROCESSED']._serialized_end=413
-  _globals['_UZIPROCESSED_NODE']._serialized_start=137
-  _globals['_UZIPROCESSED_NODE']._serialized_end=213
-  _globals['_UZIPROCESSED_SEGMENT']._serialized_start=215
-  _globals['_UZIPROCESSED_SEGMENT']._serialized_end=317
-  _globals['_UZIPROCESSED_NODEWITHSEGMENTS']._serialized_start=319
-  _globals['_UZIPROCESSED_NODEWITHSEGMENTS']._serialized_end=413
+  _globals['_UZISPLITTED']._serialized_start=42
+  _globals['_UZISPLITTED']._serialized_end=90
+  _globals['_UZIPROCESSED']._serialized_start=93
+  _globals['_UZIPROCESSED']._serialized_end=463
+  _globals['_UZIPROCESSED_NODE']._serialized_start=187
+  _globals['_UZIPROCESSED_NODE']._serialized_end=263
+  _globals['_UZIPROCESSED_SEGMENT']._serialized_start=265
+  _globals['_UZIPROCESSED_SEGMENT']._serialized_end=367
+  _globals['_UZIPROCESSED_NODEWITHSEGMENTS']._serialized_start=369
+  _globals['_UZIPROCESSED_NODEWITHSEGMENTS']._serialized_end=463
 # @@protoc_insertion_point(module_scope)
