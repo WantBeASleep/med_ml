beb8eea49245887e81d87f3158dcde330229d294
s.sokolovskii <s.sokolovskii@vk.team>
2025-05-06 23:42:28 +0300

    Fix codegenn

--- composition-api/internal/generated/http/api/oas_client_gen.go
+++ composition-api/internal/generated/http/api/oas_client_gen.go
@@ -4965,39 +4965,6 @@ func (c *Client) sendYookassaWebhooksPost(ctx context.Context, request *Yookassa
 		return res, errors.Wrap(err, "encode request")
 	}
 
-	{
-		type bitset = [1]uint8
-		var satisfied bitset
-		{
-			stage = "Security:BearerAuth"
-			switch err := c.securityBearerAuth(ctx, YookassaWebhooksPostOperation, r); {
-			case err == nil: // if NO error
-				satisfied[0] |= 1 << 0
-			case errors.Is(err, ogenerrors.ErrSkipClientSecurity):
-				// Skip this security.
-			default:
-				return res, errors.Wrap(err, "security \"BearerAuth\"")
-			}
-		}
-
-		if ok := func() bool {
-		nextRequirement:
-			for _, requirement := range []bitset{
-				{0b00000001},
-			} {
-				for i, mask := range requirement {
-					if satisfied[i]&mask != mask {
-						continue nextRequirement
-					}
-				}
-				return true
-			}
-			return false
-		}(); !ok {
-			return res, ogenerrors.ErrSecurityRequirementIsNotSatisfied
-		}
-	}
-
 	stage = "SendRequest"
 	resp, err := c.cfg.Client.Do(r)
 	if err != nil {
